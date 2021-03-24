efc597ac80077ee09233b623d2b719bbec6c50f5
Pawel Żydziak <pawel.zydziak@iteo.com>
2021-03-24 07:47:38 +0100

    improvements

--- main.cpp
+++ main.cpp
@@ -27,6 +27,40 @@ void getQuantity(const std::string& name){
 }
 #pragma endregion
 
+//// ATOMIC SP
+#pragma region
+[[deprecated]]void smart() {
+    std::shared_ptr<int> ptr = std::make_shared<int>(2011);
+    for (auto i = 0;i < 10; i++)
+    {
+        std::thread([ptr] {
+                        std::shared_ptr<int> localPtr(ptr);
+                        localPtr = std::make_shared<int>(2014);
+                    }
+        ).detach();
+    }
+
+    std::shared_ptr<int> ptr1 = std::make_shared<int>(2011);
+
+    for (auto i= 0; i<10; i++){
+        std::thread([&ptr1]{
+            ptr1= std::make_shared<int>(2014);
+        }).detach();
+    }
+
+    std::shared_ptr<int> ptr2 = std::make_shared<int>(2011);
+
+    for (auto i =0;i<10;i++){
+        std::thread([&ptr2]{
+            auto localPtr= std::make_shared<int>(2014);
+            std::atomic_store(&ptr2, localPtr);
+        }).detach();
+    }
+
+
+}
+#pragma endregion
+
 //// LATCHES AND BARRIERS
 #pragma region
 #if 0
@@ -46,10 +80,7 @@ void DoWork(threadpool* pool) {
 
 // Barrier
 void DoWork() {
-    Tasks& tasks;
-    int n_threads;
-    std::vector<thread*> workers;
-
+    ...
     std::barrier task_barrier(n_threads);
 
     for (int i = 0; i < n_threads; ++i) {
@@ -57,27 +88,24 @@ void DoWork() {
             bool active = true;
             while(active) {
                 Task task = tasks.get();
-                // perform task
+
                 ...
+
                 task_barrier.arrive_and_wait();
             }
         });
     }
-    // Read each stage of the task until all stages are complete.
     while (!finished()) {
         GetNextStage(tasks);
     }
 }
+
 // Flex barrier
 void DoWork() {
-    Tasks& tasks;
-    int initial_threads;
-    std::atomic<int> current_threads(initial_threads);
-    std::vector<thread*> workers;
 
-    // Create a flex_barrier, and set a lambda that will be
-    // invoked every time the barrier counts down. If one or more
-    // active threads have completed, reduce the number of threads.
+    ...
+    int initial_threads;
+    atomic<int> current_threads(initial_threads);
     std::function rf = [&] { return current_threads;};
     std::flex_barrier task_barrier(n_threads, rf);
 
@@ -96,8 +124,6 @@ void DoWork() {
             }
         });
     }
-
-    // Read each stage of the task until all stages are complete.
     while (!finished()) {
         GetNextStage(tasks);
     }
@@ -147,7 +173,7 @@ void functionsShowOff() {
 
 std::vector<int> getNumbers(int begin, int end, int inc = 1)
 {
-    std::vector<int> numbers;                      // (1)
+    std::vector<int> numbers;
     for (int i = begin; i < end; i += inc) {
         numbers.push_back(i);
     }
@@ -166,6 +192,44 @@ int generatorForNumbers(int begin, int inc = 1)
 //
 //}
 
+
+/* Awaitable
+std::suspend_always
+struct suspend_always {
+    constexpr bool await_ready() const noexcept { return false; }
+    constexpr void await_suspend(coroutine_handle<>) const noexcept {}
+    constexpr void await_resume() const noexcept {}
+};
+
+ struct suspend_never {
+   constexpr bool await_ready() const noexcept { return true; }
+    constexpr void await_suspend(coroutine_handle<>) const noexcept {}
+    constexpr void await_resume() const noexcept {}
+};
+
+*/
+
+/* Server C++
+Acceptor acceptor{443};
+
+while (true){
+    Socket socket= acceptor.accept();
+    auto request= socket.read();
+    auto response= handleRequest(request);
+    socket.write(response);
+}
+
+Acceptor acceptor{443};
+
+while (true){
+    Socket socket= co_await acceptor.accept();
+    auto request= co_await socket.read();
+    auto response= handleRequest(request);
+    co_await socket.write(response);
+}
+
+ */
+
 #pragma endregion
 //// TRANSACTIONAL MEMORY
 #pragma region
@@ -177,15 +241,17 @@ void increment()
     synchronized
     {
         std::cout << ++j << " ,";
-        std::this_thread::sleep_for(1ns);
+//        std::this_thread::sleep_for(1ns);
     }
 }
 
 #pragma endregion
-//// TASK BLOCKS
 
+//// TASK BLOCKS
+//TODO
 int main()
 {
+
 #pragma region RW LOCK
 
     std::cout << std::endl;
@@ -222,12 +288,13 @@ int main()
     std::cout << std::endl;
 
 #pragma endregion
+
 #pragma region COROUTINES
     // funkcje
     functionsShowOff();
     // Generator po staremu
     std::cout << "Po staremu "<< std::endl;
-    const auto numbers= getNumbers(-10, 11);
+    const auto numbers = getNumbers(-10, 11);
     for (auto n: numbers) std::cout << n << " ";
     std::cout << "\n\n";
     for (auto n: getNumbers(0, 101, 5)) std::cout << n << " ";
@@ -235,30 +302,28 @@ int main()
 
     // Generator po nowemu
     std::cout << std::endl;
-    const auto numbersNew= generatorForNumbers(-10);             // (2)
-    for (int i= 1; i <= 20; ++i) std::cout << numbersNew << " ";       // (4)
-//    for (auto n : generatorForNumbers(0, 5)) std::cout << n << " ";  // (3)
+    const auto numbersNew= generatorForNumbers(-10);
+    for (int i= 1; i <= 20; ++i) std::cout << numbersNew << " ";
+//    for (auto n : generatorForNumbers(0, 5)) std::cout << n << " ";
     std::cout << "\n\n";
 
 #pragma endregion
 #pragma region Transactional
     // synchronized
-std::cout << std::endl;
-std::vector<std::thread> vecSyn(10);
-std::vector<std::thread> vecUnsyn(10);
+    std::cout << std::endl;
+    std::vector<std::thread> vecSyn(10);
+//    std::vector<std::thread> vecUnsyn(10);
 
-for(auto& thr: vecSyn)
-    thr = std::thread([]{ for(int n = 0; n < 10; ++n) increment(); });
-for(auto& thr: vecUnsyn)
-    thr = std::thread([]{ for(int n = 0; n < 10; ++n) std::cout << ++j << " ,"; });
+    for(auto& thr: vecSyn)
+        thr = std::thread([]{ for(int n = 0; n < 10; ++n) increment(); });
+//    for(auto& thr: vecUnsyn)
+//        thr = std::thread([]{ for(int n = 0; n < 10; ++n) std::cout << ++j << " ,"; });
 
-for(auto& thr: vecSyn) thr.join();
-for(auto& thr: vecUnsyn) thr.join();
+    for(auto& thr: vecSyn) thr.join();
+//    for(auto& thr: vecUnsyn) thr.join();
 
-std::cout << "\n\n";
+    std::cout << "\n\n";
 #pragma endregion
 
-//std::map<std::string, int> mojaMapa = {{"pierwszy",1}, {"drugi",2}};
-//std::cout << mojaMapa["pierwszy"] << std::endl;
-//std::cout << mojaMapa["trzeci"] << std::endl;
+
 }
